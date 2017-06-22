0c87a8139329c15b7d551c486d51f9f4119d1948
Takashi Ueno <ueno.takashi.75w@st.kyoto-u.ac.jp>
2017-06-22 16:57:50 +0900

    Add keyboard input when using Oculus

--- oculus/polyVR_oc.js
+++ oculus/polyVR_oc.js
@@ -14,9 +14,15 @@ function init() {
   const groups = [groupB, groupR, groupY];
   const scene = new THREE.Scene();
 
-  const divide = 1000;
-  const angle = 2 * Math.PI / divide;
-  let rot = 0;
+  let st = 0;
+  const state = [1, 1.5, 2, 2.5, 3, 5-GR, 4];
+  let rotationX = 0;
+  let rotationY = 0;
+  let rotationZ = 0;
+  let visible_blue = true;
+  let visible_red = true;
+  let visible_yellow = true;
+  let visible_axis = false;
 
 
   renderer = new THREE.WebGLRenderer();
@@ -42,26 +48,170 @@ function init() {
     document.body.appendChild(WEBVR.getButton(effect));
   }
 
+  const axis = new THREE.AxisHelper(450);
+  scene.add(axis);
+
   setupArray();
   paintComponent();
+
+  window.addEventListener("keydown", function(e) {
+    // change state (t or space)
+    if (e.keyCode == 84 || e.keyCode == 32) {
+      st = (st + 1) % 7;
+      t = state[st];
+      reset();
+      setupArray();
+
+      // remove all existing lines
+      for (let i = 1; i <= 3; i++) {
+        while (scene.children[i].children.length > 0) {
+          scene.children[i].remove(scene.children[i].children[0]);
+        }
+      }
+
+      paintComponent();
+    }
+
+    // move
+    // x plus (h or left)
+    if (e.keyCode == 72 || e.keyCode == 37) {
+      scene.position.x += 10;
+    }
+    // x minus (l or right)
+    if (e.keyCode == 76 || e.keyCode == 39) {
+      scene.position.x -= 10;
+    }
+    // y plus (k or up)
+    if (e.keyCode == 75 || e.keyCode == 38) {
+      scene.position.y += 10;
+    }
+    // y minus (j or down)
+    if (e.keyCode == 74 || e.keyCode == 40) {
+      scene.position.y -= 10;
+    }
+    // z plus (p or PageDown)
+    if (e.keyCode == 80 || e.keyCode == 34) {
+      scene.position.z += 10;
+    }
+    // z minus (n or PageUp)
+    if (e.keyCode == 78 || e.keyCode == 33) {
+      scene.position.z -= 10;
+    }
+
+    // rotation
+    // x plus (i)
+    if (e.keyCode == 73) {
+      if (rotationX > 0) {
+        rotationX = 0;
+      } else {
+        rotationX = 1;
+      }
+    }
+    // x minus (u)
+    if (e.keyCode == 85) {
+      if (rotationX < 0) {
+        rotationX = 0;
+      } else {
+        rotationX = -1;
+      }
+    }
+    // y plus (o)
+    if (e.keyCode == 79) {
+      if (rotationY > 0) {
+        rotationY = 0;
+      } else {
+        rotationY = 1;
+      }
+    }
+    // y minus (y)
+    if (e.keyCode == 89) {
+      if (rotationY < 0) {
+        rotationY = 0;
+      } else {
+        rotationY = -1;
+      }
+    }
+    // z plus (m)
+    if (e.keyCode == 77) {
+      if (rotationZ > 0) {
+        rotationZ = 0;
+      } else {
+        rotationZ = 1;
+      }
+    }
+    // z minus (b)
+    if (e.keyCode == 66) {
+      if (rotationZ < 0) {
+        rotationZ = 0;
+      } else {
+        rotationZ = -1;
+      }
+    }
+
+    // visible
+    // blue (z)
+    if (e.keyCode == 90) {
+      if (visible_blue) {
+        visible_blue = false;
+      } else {
+        visible_blue = true;
+      }
+    }
+    // red (x)
+    if (e.keyCode == 88) {
+      if (visible_red) {
+        visible_red = false;
+      } else {
+        visible_red = true;
+      }
+    }
+    // yellow (c)
+    if (e.keyCode == 67) {
+      if (visible_yellow) {
+        visible_yellow = false;
+      } else {
+        visible_yellow = true;
+      }
+    }
+    // axis (v)
+    if (e.keyCode == 86) {
+      if (visible_axis) {
+        visible_axis = false;
+      } else {
+        visible_axis = true;
+      }
+    }
+  });
 
   document.getElementById("WebGL-output").appendChild(renderer.domElement);
   render();
 
 
   function render() {
-    if (rot < divide) {
-      scene.rotation.y += angle;
-      rot++;
-    } else if (rot < 2 * divide) {
-      scene.rotation.x += angle;
-      rot++;
-    } else {
-      rot = 0;
-    }
-
     const delta = clock.getDelta();
     controls.update(delta);
+
+    if (rotationX > 0) {
+      scene.rotation.x += 0.01;
+    } else if (rotationX < 0) {
+      scene.rotation.x -= 0.01;
+    }
+    if (rotationY > 0) {
+      scene.rotation.y += 0.01;
+    } else if (rotationY < 0) {
+      scene.rotation.y -= 0.01;
+    }
+    if (rotationZ > 0) {
+      scene.rotation.z += 0.01;
+    } else if (rotationZ < 0) {
+      scene.rotation.z -= 0.01;
+    }
+
+
+    groupB.visible = visible_blue;
+    groupR.visible = visible_red;
+    groupY.visible = visible_yellow;
+    axis.visible = visible_axis;
 
     requestAnimationFrame(render);
     effect.render(scene, camera);
